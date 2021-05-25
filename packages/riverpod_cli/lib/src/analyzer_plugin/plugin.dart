13b16539fb536e79aba52888b02ea55d1f1297c7
Tim Whiting <tim@whitings.org>
2021-05-24 22:25:16 -0600

    attempt at getting linter working

--- packages/riverpod_cli/lib/src/analyzer_plugin/plugin.dart
+++ packages/riverpod_cli/lib/src/analyzer_plugin/plugin.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+import 'dart:isolate';
+
 import 'package:analyzer/dart/analysis/results.dart';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/file_system/file_system.dart';
+import 'package:analyzer/file_system/physical_file_system.dart';
 // ignore: implementation_imports
 import 'package:analyzer/src/context/builder.dart';
 // ignore: implementation_imports
@@ -13,6 +17,12 @@ import 'package:analyzer_plugin/channel/channel.dart';
 import 'package:analyzer_plugin/plugin/plugin.dart';
 import 'package:analyzer_plugin/protocol/protocol_common.dart' as plugin;
 import 'package:analyzer_plugin/protocol/protocol_generated.dart' as plugin;
+import 'package:analyzer_plugin/starter.dart';
+
+void start(List<String> args, SendPort sendPort) {
+  ServerPluginStarter(RiverpodAnalysisPlugin(PhysicalResourceProvider.INSTANCE))
+      .start(sendPort);
+}
 
 class RiverpodAnalysisPlugin extends ServerPlugin {
   RiverpodAnalysisPlugin(ResourceProvider? provider) : super(provider);
@@ -20,6 +30,7 @@ class RiverpodAnalysisPlugin extends ServerPlugin {
   final Checker checker = Checker();
   @override
   AnalysisDriverGeneric createAnalysisDriver(plugin.ContextRoot contextRoot) {
+    print('Hi');
     final rootPath = contextRoot.root;
     final root = ContextRoot(
       rootPath,
@@ -40,28 +51,51 @@ class RiverpodAnalysisPlugin extends ServerPlugin {
     );
 
     final dartDriver = contextBuilder.buildDriver(root, workspace);
-    dartDriver.results.listen(_processResult);
+    runZonedGuarded(
+      () {
+        dartDriver.results.listen(_processResult);
+      },
+      (e, stackTrace) {
+        channel.sendNotification(
+          plugin.PluginErrorParams(false, e.toString(), stackTrace.toString())
+              .toNotification(),
+        );
+      },
+    );
     return dartDriver;
   }
 
   @override
-  List<String> get fileGlobsToAnalyze => <String>['**/*.dart'];
+  List<String> get fileGlobsToAnalyze => const ['*.dart'];
 
   @override
   String get name => 'riverpod analyzer';
 
   @override
-  String get version => '0.0.1';
+  String get version => '1.0.0-alpha.0';
 
   /// Computes errors based on an analysis result and notifies the analyzer.
   // ignore: deprecated_member_use
   void _processResult(ResolvedUnitResult analysisResult) {
     // If there is no relevant analysis result, notify the analyzer of no errors.
-    if (analysisResult.unit != null) {
-      final checkResult = checker.check(analysisResult.libraryElement, channel);
-      channel.sendNotification(plugin.AnalysisErrorsParams(
-              analysisResult.path!, checkResult.keys.toList())
-          .toNotification());
+    try {
+      if (analysisResult.unit != null) {
+        final checkResult =
+            checker.check(analysisResult.libraryElement, channel);
+        channel.sendNotification(plugin.AnalysisErrorsParams(
+                analysisResult.path!, checkResult.keys.toList())
+            .toNotification());
+      } else {
+        channel.sendNotification(
+          plugin.AnalysisErrorsParams(analysisResult.path!, [])
+              .toNotification(),
+        );
+      }
+    } on Exception catch (e, stackTrace) {
+      channel.sendNotification(
+        plugin.PluginErrorParams(false, e.toString(), stackTrace.toString())
+            .toNotification(),
+      );
     }
   }
 
@@ -80,22 +114,26 @@ class Checker {
         final error = compilationUnit.library.imports.firstWhere(
             (element) => element.uri?.contains('riverpod') ?? false);
         final lineInfo = compilationUnit.lineInfo?.getLocation(error.uriOffset);
-        final lineEndInfo = compilationUnit.lineInfo?.getLocation(error.uriEnd);
+        final lineEndInfo =
+            compilationUnit.lineInfo?.getLocation(error.uriOffset + 5);
 
         final err = plugin.AnalysisError(
-            plugin.AnalysisErrorSeverity.ERROR,
-            plugin.AnalysisErrorType.LINT,
-            plugin.Location(
-              compilationUnit.source.fullName,
-              error.uriOffset,
-              error.uri!.length,
-              lineInfo!.lineNumber,
-              lineInfo.columnNumber,
-              lineEndInfo!.lineNumber,
-              lineEndInfo.columnNumber,
-            ),
-            'Fix this',
-            'RIVERPOD_IMPORT_NEEDS_FIXES');
+          plugin.AnalysisErrorSeverity.INFO,
+          plugin.AnalysisErrorType.LINT,
+          plugin.Location(
+            compilationUnit.source.fullName,
+            error.uriOffset + 1,
+            error.uri!.length,
+            lineInfo!.lineNumber,
+            lineInfo.columnNumber,
+            lineEndInfo!.lineNumber,
+            lineEndInfo.columnNumber,
+          ),
+          'Fix this',
+          'riverpod_imports',
+          hasFix: false,
+          url: compilationUnit.uri,
+        );
         final fix = plugin.PrioritizedSourceChange(
             1000000,
             plugin.SourceChange(
